adaa0c9e146da070aff041def7b3b5b9b306be90
Alex-Jr <50220189+Alex-Jr@users.noreply.github.com>
2021-09-27 12:25:24 -0300

    Minimal bot

--- src/bot.ts
+++ src/bot.ts
@@ -1,1 +1,14 @@
-const a = ""
+import 'dotenv/config'
+import { Client, Intents } from 'discord.js';
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on('messageCreate', async message => {
+  console.log(message)
+});
+
+client.login(process.env.TOKEN);
