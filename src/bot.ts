93721abb1b75482ce8f527f133a204e0a054619a
Alex-Jr <50220189+Alex-Jr@users.noreply.github.com>
2021-09-28 09:28:10 -0300

    Exporting metrics with prometheus

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,12 @@
 import 'dotenv/config'
 import { Client, Intents } from 'discord.js';
+import http from 'http';
+import Prometheus from 'prom-client';
+
+const messages_received = new Prometheus.Counter({
+  name: 'messages_received',
+  help: 'Counter for messages received'
+})
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
@@ -9,6 +16,16 @@ client.on('ready', () => {
 
 client.on('messageCreate', async message => {
   console.log(message)
+
+  messages_received.inc(1)
 });
 
 client.login(process.env.TOKEN);
+
+
+const server = http.createServer(async (req, res) => {
+  res.setHeader('Content-Type', Prometheus.register.contentType);
+  res.end(await Prometheus.register.metrics())
+})
+
+server.listen(9000)
