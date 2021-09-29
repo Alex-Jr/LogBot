0f3730e4146ea181824114ef6dd77a7a7671cf57
Alex-Jr <50220189+Alex-Jr@users.noreply.github.com>
2021-09-29 19:46:46 -0300

    Using discord guide and refactoring it

--- src/classes/Track.ts
+++ src/classes/Track.ts
@@ -1,3 +1,87 @@
-export default class Track {
-    
+import { getInfo } from 'ytdl-core';
+import { AudioResource, createAudioResource, demuxProbe } from '@discordjs/voice';
+import { raw as ytdl } from 'youtube-dl-exec';
+import noop from '../utils/noop';
+
+export interface TrackData {
+	url: string;
+	title: string;
+	onStart: () => void;
+	onFinish: () => void;
+	onError: (error: Error) => void;
+}
+
+export default class Track implements TrackData {
+    public readonly title: string;
+    public readonly url: string;
+	public readonly onStart: () => void;
+	public readonly onFinish: () => void;
+	public readonly onError: (error: Error) => void;
+
+    private constructor({ url, title, onStart, onFinish, onError }: TrackData) {
+		this.url = url;
+		this.title = title;
+		this.onStart = onStart;
+		this.onFinish = onFinish;
+		this.onError = onError;
+	}
+
+    public createAudioResource(): Promise<AudioResource<Track>> {
+		return new Promise((resolve, reject) => {
+			const process = ytdl(
+				this.url,
+				{
+					o: '-',
+					q: '',
+					f: 'bestaudio[ext=webm+acodec=opus+asr=48000]/bestaudio',
+					r: '100K',
+				},
+				{ stdio: ['ignore', 'pipe', 'ignore'] },
+			);
+			if (!process.stdout) {
+				reject(new Error('No stdout'));
+				return;
+			}
+			const stream = process.stdout;
+			const onError = (error: Error) => {
+				if (!process.killed) process.kill();
+				stream.resume();
+				reject(error);
+			};
+			process
+				.once('spawn', () => {
+					demuxProbe(stream)
+						.then((probe) => resolve(createAudioResource(probe.stream, { metadata: this, inputType: probe.type })))
+						.catch(onError);
+				})
+				.catch(onError);
+		});
+	}
+
+    public static async from(url: string, methods: Pick<Track, 'onStart' | 'onFinish' | 'onError'>): Promise<Track> {
+		const info = await getInfo(url);
+
+		// The methods are wrapped so that we can ensure that they are only called once.
+		const wrappedMethods = {
+			onStart() {
+				wrappedMethods.onStart = noop;
+				methods.onStart();
+			},
+			onFinish() {
+				wrappedMethods.onFinish = noop;
+				methods.onFinish();
+			},
+			onError(error: Error) {
+				wrappedMethods.onError = noop;
+				methods.onError(error);
+			},
+		};
+
+		return new Track({
+			title: info.videoDetails.title,
+			url,
+			...wrappedMethods,
+		});
+	}
+
 }
--- src/commands/music.ts
+++ src/commands/music.ts
@@ -1,97 +1,200 @@
 import 'dotenv/config'
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { joinVoiceChannel } from '@discordjs/voice';
-import { GuildMember, Message } from 'discord.js';
+import { AudioPlayerStatus, AudioResource, entersState, joinVoiceChannel, VoiceConnectionStatus } from '@discordjs/voice';
+import { GuildMember, Snowflake } from 'discord.js';
 import type { CommandInteraction } from 'discord.js';
 import type Command from "../interfaces/Command"
-import { default as ytsearch } from 'youtube-search';
+import MusicSubscription from '../classes/MusicSubscription';
+import Track from '../classes/Track';
 
+const subscriptions = new Map<Snowflake, MusicSubscription>();
 
 const data = new SlashCommandBuilder()
 	.setName('music')
 	.setDescription('Play youtube music and playlists!')
   .addSubcommand(subcommand => 
     subcommand
-      .setName('queue')
-      .setDescription('Enqueue a music')
+      .setName('play')
+      .setDescription('Play a music')
       .addStringOption((option) => 
         option
-          .setName('name')
+          .setName('song')
           .setDescription('Music name')
           .setRequired(true)
       )
   )
+  .addSubcommand(subcommand => 
+    subcommand
+      .setName('skip')
+      .setDescription('Skip a music')  
+  )
+  .addSubcommand(subcommand => 
+    subcommand
+      .setName('queue')
+      .setDescription('Show musics in queue')  
+  )
+  .addSubcommand(subcommand => 
+    subcommand
+      .setName('pause')
+      .setDescription('Pause player')  
+  )
+  .addSubcommand(subcommand => 
+    subcommand
+    .setName('resume')
+    .setDescription('Resume player')
+  )
+  .addSubcommand(subcommand => 
+    subcommand
+      .setName('leave')
+      .setDescription('Leave')  
+  );
 
-async function searchMusic(name: string) {
-  const query = await ytsearch(name, {
-    key: process.env.YTKEY,
-    maxResults: 5,
-  })
+async function play(interaction: CommandInteraction, subscription: MusicSubscription | undefined) {
+  // Extract the video URL from the command
+  const url = interaction.options.get('song')!.value! as string;
 
-  return query.results
-}
-
-async function queue(interaction: CommandInteraction): Promise<void> {
-  const musicName = interaction.options.getString('name')
-
-  if(!musicName) return;
-
-  const results = await searchMusic(musicName);
-
-  let msg = '';
-
-  results.forEach((result, index) => {
-    msg += `${index + 1}° - ${result.title}\n`
-  })
-
-  const message = await interaction.editReply(msg);
-  if(!(message instanceof Message)) return;
-
-  const reactions = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣']
-
-
-  for(const reaction of reactions) {
-    await message.react(reaction);
+  // If a connection to the guild doesn't already exist and the user is in a voice channel, join that channel
+  // and create a subscription.
+  if (!subscription) {
+    if (interaction.member instanceof GuildMember && interaction.member.voice.channel) {
+      const channel = interaction.member.voice.channel;
+      subscription = new MusicSubscription(
+        joinVoiceChannel({
+          channelId: channel.id,
+          guildId: channel.guild.id,
+          adapterCreator: channel.guild.voiceAdapterCreator,
+        }),
+      );
+      subscription.voiceConnection.on('error', console.warn);
+      subscriptions.set(interaction.guildId!, subscription);
+    }
   }
 
-  function filter (reaction: any, user: any) {
-    console.log(reaction);
-    return reactions.includes(reaction.emoji.name) && user.id === interaction.user.id;
+  // If there is no subscription, tell the user they need to join a channel.
+  if (!subscription) {
+    await interaction.followUp('Join a voice channel and then try that again!');
+    return;
   }
-  
+
+  // Make sure the connection is ready before processing the user's request
   try {
-    const collected = await message.awaitReactions({ filter, max: 1, time: 20000, errors: ['time'] })
+    await entersState(subscription.voiceConnection, VoiceConnectionStatus.Ready, 20e3);
+  } catch (error) {
+    console.warn(error);
+    await interaction.followUp('Failed to join voice channel within 20 seconds, please try again later!');
+    return;
+  }
 
-    const reactionSelected = collected.first();
-    
-    const index = reactions.findIndex((reaction) => reaction === reactionSelected?.emoji.name);
-    await message.edit(`Escolhida: ${results[index].title}`);
-  } catch (err) {
-    await message.react('Nenhuma resposta...')
+  try {
+    // Attempt to create a Track from the user's video URL
+    const track = await Track.from(url, {
+      onStart() {
+        interaction.followUp({ content: 'Now playing!' }).catch(console.warn);
+      },
+      onFinish() {
+        interaction.followUp({ content: 'Now finished!' }).catch(console.warn);
+      },
+      onError(error) {
+        console.warn(error);
+        interaction.followUp({ content: `Error: ${error.message}`, ephemeral: true }).catch(console.warn);
+      },
+    });
+    // Enqueue the track and reply a success message to the user
+    subscription.enqueue(track);
+    await interaction.followUp(`Enqueued **${track.title}**`);
+  } catch (error) {
+    console.warn(error);
+    await interaction.editReply('Failed to play track, please try again later!');
+  }
+} 
+
+async function skip(interaction: CommandInteraction, subscription: MusicSubscription | undefined) {
+  if (subscription) {
+    // Calling .stop() on an AudioPlayer causes it to transition into the Idle state. Because of a state transition
+    // listener defined in music/subscription.ts, transitions into the Idle state mean the next track from the queue
+    // will be loaded and played.
+    subscription.audioPlayer.stop();
+    await interaction.editReply('Skipped song!');
+  } else {
+    await interaction.editReply('Not playing in this server!');
   }
 }
 
+async function queue(interaction: CommandInteraction, subscription: MusicSubscription | undefined) {
+  // Print out the current queue, including up to the next 5 tracks to be played.
+  if (subscription) {
+    const current =
+      subscription.audioPlayer.state.status === AudioPlayerStatus.Idle
+        ? `Nothing is currently playing!`
+        : `Playing **${(subscription.audioPlayer.state.resource as AudioResource<Track>).metadata.title}**`;
 
+    const queue = subscription.queue
+      .slice(0, 5)
+      .map((track, index) => `${index + 1}) ${track.title}`)
+      .join('\n');
+
+    await interaction.editReply(`${current}\n\n${queue}`);
+  } else {
+    await interaction.editReply('Not playing in this server!');
+  }
+}
+
+async function pause(interaction: CommandInteraction, subscription: MusicSubscription | undefined) {
+  if (subscription) {
+    subscription.audioPlayer.pause();
+    await interaction.editReply({ content: `Paused!` });
+  } else {
+    await interaction.editReply('Not playing in this server!');
+  }
+}
+
+async function resume(interaction: CommandInteraction, subscription: MusicSubscription | undefined) {
+  if (subscription) {
+    subscription.audioPlayer.unpause();
+    await interaction.editReply({ content: `Unpaused!` });
+  } else {
+    await interaction.editReply('Not playing in this server!');
+  }
+}
+
+async function leave(interaction: CommandInteraction, subscription: MusicSubscription | undefined) {
+  if (subscription) {
+    subscription.voiceConnection.destroy();
+    subscriptions.delete(interaction.guildId!);
+    await interaction.editReply({ content: `Left channel!` });
+  } else {
+    await interaction.editReply('Not playing in this server!');
+  }
+}
 
 async function execute(interaction: CommandInteraction): Promise<void> {
-	if(!interaction.member || !(interaction.member instanceof GuildMember)) return;
+  if(!interaction.guildId) return
 
-  const channel = interaction.member.voice.channel
-  if(!channel) return interaction.reply('Você não está em um canal de voz');
-
-  joinVoiceChannel({
-    channelId: channel.id,
-    guildId: channel.guild.id,
-    adapterCreator: channel.guild.voiceAdapterCreator
-  })
+  const subscription = subscriptions.get(interaction.guildId);
 
   await interaction.deferReply();
 
   switch(interaction.options.getSubcommand()) {
-    case 'queue': 
-      await queue(interaction);
+    case 'play': 
+      await play(interaction, subscription);
+      break;
+    case 'skip':
+      await skip(interaction, subscription);
+      break;
+    case 'queue':
+      await queue(interaction, subscription);
+      break;
+    case 'pause':
+      await pause(interaction, subscription)
+      break
+    case 'resume':
+      await resume(interaction, subscription)
+      break;
+    case 'leave':
+      await leave(interaction, subscription)
       break;
     default:
+      await interaction.editReply('Unknown command');
       break;
   }
 
